5b4b5ef7d9f60ac8c71ac366254bc56256b73fbf
Hemant Joshi <hd4207577@gmail.com>
2020-10-30 08:58:02 +0530

    Txs fix

---
+++ src/entities/Post.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { ObjectType, Field, Int } from "type-graphql";
+
+@ObjectType()
+@Entity()
+export class Post {
+  @Field()
+  @PrimaryKey()
+  id!: number;
+
+  @Field()
+  @Property({ type: "date" })
+  createdAt = new Date();
+
+  @Field()
+  @Property({ type: "date", onUpdate: () => new Date() })
+  updatedAt = new Date();
+
+  @Field()
+  @Property({ type: "text" })
+  title!: string;
+}
